3643bd461e3589ba38a0faba9074ca51dd3187b0
rochaa6 <104039970+rochaa6@users.noreply.github.com>
2022-04-19 19:52:57 -0400

    Add files via upload

---
+++ session.php
@@ -0,0 +1,18 @@
+<?php
+   include('config.php');
+   session_start();
+   
+  // $demo_check = $_SESSION['demo_user'];
+/*   
+   $ses_sql = mysqli_query($db,"select id from demologin where password = '$demo_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['password'];
+   
+   if(!isset($_SESSION['demo_user'])){
+      header("location:../TestLogin.php");
+      die();
+   }
+*/
+?>
